a6fbd496e82bd43339787a55651ae5297ffe4c1a
choco1125 <sebascruz981125@gmail.com>
2020-08-09 12:59:06 -0500

    Se corrige el mensaje de eliminar modalidad

--- src/components/admin/modalidades/eliminar.js
+++ src/components/admin/modalidades/eliminar.js
@@ -1,65 +1,75 @@
-import React,{useState} from 'react';
-import Spinner from '../../spinner';
-import consumidor from '../../../helpers/consumidor';
-import $ from 'jquery';
+import React, { useState } from "react";
+import Spinner from "../../spinner";
+import consumidor from "../../../helpers/consumidor";
+import $ from "jquery";
 
+const Eliminar = ({ datos, alerta, update }) => {
+  const [spinner, setSpinner] = useState(false);
 
-const Eliminar = ({datos,alerta,update}) => {
+  const del = async () => {
+    setSpinner(true);
 
-    const [spinner, setSpinner] = useState(false);
+    let res = await consumidor.delete("modalities", datos.id);
+    if (res === "Modalidad eliminada") {
+      await update();
+      alerta(res, "success");
+    } else if (res.message) {
+      alerta(res.message, "danger");
+    } else {
+      alerta(JSON.stringify(res), "danger");
 
-    const del = async ()=>{
-        setSpinner(true);
-        
-        let res =  await consumidor.delete('modalities',datos.id);
-        if(res === 'Modalidad eliminada'){
-            await update();
-            alerta(res,'success');
-        }else if(res.message){
-            alerta(res.message,'danger');
-        }else{
-            alerta(JSON.stringify(res),'danger');
-
-            console.log(res)
-        }
-
-        $('#eliminar').modal('hide');
-
-        setSpinner(false);
+      console.log(res);
     }
 
-    return (
-        <div className="modal fade" id="eliminar" role="dialog"
-            aria-labelledby="eliminarLabel" aria-hidden="true">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="eliminarLabel">Eliminar rol</h5>
-                        <button type="button" className="close" 
-                            data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        ¿Deseas eliminar la modalidad?
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button"
-                            className="btn btn-outline-secondary"
-                            data-dismiss="modal"
-                        >
-                            Cancelar
-                            </button>
-                        <button type="button"
-                            className="btn btn-outline-danger"
-                            onClick={() => del()}>
-                            Sí, eliminar <Spinner show={spinner} />
-                        </button>
-                    </div>
-                </div>
-            </div>
+    $("#eliminar").modal("hide");
+
+    setSpinner(false);
+  };
+
+  return (
+    <div
+      className="modal fade"
+      id="eliminar"
+      role="dialog"
+      aria-labelledby="eliminarLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="eliminarLabel">
+              Eliminar Modalidad
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">¿Deseas eliminar la modalidad?</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              data-dismiss="modal"
+            >
+              Cancelar
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={() => del()}
+            >
+              Sí, eliminar <Spinner show={spinner} />
+            </button>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Eliminar;
